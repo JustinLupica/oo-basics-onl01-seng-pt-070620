418fa0c2e0efaff29c327fd81b1d7f2bfedee38e
Justin Lupica <justinlupica@gmail.com>
2020-07-23 17:31:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class
 
 
+
+
+class Shoe 
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  
+end
