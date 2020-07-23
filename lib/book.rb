a45359f6ff7cdbf9c1e609e696297bc81eab34ff
Justin Lupica <justinlupica@gmail.com>
2020-07-23 17:34:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,12 @@
-class
-
-
-
-
-class Shoe 
-  attr_reader :brand
-  attr_accessor :color, :size, :material, :condition
-  def initialize(brand)
-    @brand = brand
+class Book
+  attr_accessor :author, :page_count, :genre 
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title 
   end
   
-  def cobble
-    puts "Your shoe is as good as new!"
-    @condition = "new"
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-  
 end
